f13d11ddb4abc6d21c0fb6f6db039c2ee68e0647
Lucas Costa <lucasfrct@gmail.com>
2024-01-29 19:07:54 -0300

    ajustando a rota health

--- src/main/java/com/onboarding/user/onboardinguser/OnboardingUserApplication.java
+++ src/main/java/com/onboarding/user/onboardinguser/OnboardingUserApplication.java
@@ -1,15 +1,23 @@
 package com.onboarding.user.onboardinguser;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityDataConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OnboardingUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnboardingUserApplication.class, args);
 	}
+
+	 @GetMapping("/")
+    String index() {
+        return "ON";
+    }
+
+
 
 }
